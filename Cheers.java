1b68621deaad81ed53a07d2279bbea1c3c862c47
avi sit <avraham.sitton@post.runi.ac.il>
2024-11-14 23:32:36 +0200

    subSubmittingAnAssignment

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,44 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String toSpell = args[0];
+            int repeat = Integer.parseInt(args[1]);
+            String toPrint = "";
+
+            for(int i = 0; i < toSpell.length(); i++){
+                char a = Character.toUpperCase(args[0].charAt(i));
+                System.out.println("Give me an " + a + ": " + a + "!");
+                toPrint += a;
+
+            }
+            System.out.println("What does that spell?");
+            
+            
+            for(int i = 0; i <= repeat; i++){
+                System.out.println( toPrint + "!!!");
+            }
+
+
+
         }
 }
+
+            
+            
+            
+            
+            
+            
+/* Give me an R: R!
+Give me a U: U!
+Give me an N: N!
+Give me an I: I!
+What does that spell?
+RUNI!!!
+RUNI!!!
+RUNI!!!
+RUNI!!!
+RUNI!!!
+        }
+}
+*/
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,19 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+	    int correctH = Integer.parseInt( "" + args[0].charAt(0) + args[0].charAt(1));
+        int correctM = Integer.parseInt( "" + args[0].charAt(3) + args[0].charAt(4));
+        int minToAdd = Integer.parseInt( "" + args[1]);
+        int h = correctH * 60;
+        int m = correctM;
+        int totalMin = (h + m + minToAdd);
+        int mm = (totalMin % 60);
+        int hh = ((totalMin - mm) / 60);
+
+        if((hh > 23) && ((hh % 24) < 10)){
+            System.out.println("0" + hh % 24 + ":" + mm);
+        } else System.out.println( hh + ":" + mm);
+        // System.err.println(hh + " " + correctM );
+        }
+
     }
-}
+
