fc94ba1422041d0fb42a5a1cd08c91b03dc43be2
AvrahamSitton <avraham.sitton@post.runi.ac.il>
2024-11-16 11:42:44 +0200

    add

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,16 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int seed = Integer.parseInt(args[0]);
+		while (seed > 1) {
+			if (seed % 2 == 0) {
+				seed /= 2;
+			} else {
+				seed = seed * 3 + 1;
+			}
+			System.out.print(seed + ", ");
+
+		}
+
 	}
 }
