f0660e2d5bb09bcf0e75b54d3ad30cae125e4c10
AvrahamSitton <avraham.sitton@post.runi.ac.il>
2024-11-16 11:50:02 +0200

    add

--- Cheers.java
+++ Cheers.java
@@ -18,7 +18,7 @@ public class Cheers {
         }
         System.out.println("What does that spell?");
 
-        for (int i = 0; i <= repeat; i++) {
+        for (int i = 0; i < repeat; i++) {
             System.out.println(toPrint + "!!!");
         }
 
