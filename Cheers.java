847aae1d8aa20c5513333344a558eda031a067df
AvrahamSitton <avraham.sitton@post.runi.ac.il>
2024-11-16 11:42:17 +0200

    add

--- Cheers.java
+++ Cheers.java
@@ -1,44 +1,26 @@
 // Prints a crowd cheering output.
 public class Cheers {
-        public static void main(String[] args) {
-	    String toSpell = args[0];
-            int repeat = Integer.parseInt(args[1]);
-            String toPrint = "";
-
-            for(int i = 0; i < toSpell.length(); i++){
-                char a = Character.toUpperCase(args[0].charAt(i));
+    public static void main(String[] args) {
+        String toSpell = args[0];
+        int repeat = Integer.parseInt(args[1]);
+        String toPrint = "";
+        String aORan = "";
+        
+        for (int i = 0; i < toSpell.length(); i++) {
+            char a = Character.toUpperCase(args[0].charAt(i));
+            if ("AEFHILMNORSX".indexOf(a) > -1){
                 System.out.println("Give me an " + a + ": " + a + "!");
-                toPrint += a;
-
+            } else {
+                System.out.println("Give me a " + a + ": " + a + "!");
             }
-            System.out.println("What does that spell?");
-            
-            
-            for(int i = 0; i <= repeat; i++){
-                System.out.println( toPrint + "!!!");
-            }
-
-
+            toPrint += a;
 
         }
-}
+        System.out.println("What does that spell?");
 
-            
-            
-            
-            
-            
-            
-/* Give me an R: R!
-Give me a U: U!
-Give me an N: N!
-Give me an I: I!
-What does that spell?
-RUNI!!!
-RUNI!!!
-RUNI!!!
-RUNI!!!
-RUNI!!!
+        for (int i = 0; i <= repeat; i++) {
+            System.out.println(toPrint + "!!!");
         }
+
+    }
 }
-*/
